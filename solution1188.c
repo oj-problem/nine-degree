00be7f21206b5388af3d617ffe418f6744dedbbd
hellfiresong <hellfiresong.me@gmail.com>
2016-01-17 15:50:06 +0800

    Josephus ring problem

--- solution1188.c
+++ solution1188.c
@@ -5,50 +5,46 @@
  * hellfire (asyncloading#163.com)
  * Jan 17th, 2016
  */
+#include<stdio.h>
 
+int main(int argc, char** argv)
+{
+  int n;                      /* Total number quantity.*/
+  int p;                      /* Given detail number.*/
 
+  while (scanf("%d %d", &n, &p) != EOF)
+  {
+    int i = 0;                  /* Loop variable */
+    int num[3000] = {0};        /* IS die? 0 stands live, 1 stands die. */                   
+    int counter = 0;            /* Counter */
+    int k = 0;                  /* Die people count*/
 
-
-
-/**
- *  This general method complexicty is O(nm), Will cause Time Limte Exceed.
- *  Silly method do not recommend.....
- */
-
-//#include<stdio.h>
-
-//int main(int argc, char** argv)
-//{
-//  int n;                      /* Total number quantity.*/
-//  int p;                      /* Given detail number.*/
-
-//  int i = 0;                  /* Loop variable */
-//  int num[3000] = {0};        /* IS die? 0 stands live, 1 stands die. */                   
-//  int counter = 0;            /* Counter */
-//  int k = 0;                  /* Die people count*/
-
-//  while (scanf("%d %d", &n, &p) != EOF)
-//  {
-//    do
-//    {
-//      /* Mock ring, join first number and last number together. */
-//      ++i;
-//      if (i > n)
-//      {
-//        i = 1;
-//      }
+    do
+    {
+      /* Mock ring, join first number and last number together. */
+      ++i;
+      if (i > n)
+      {
+        i = 1;
+      }
       
-//      if (!num[i])
-//      {
-//        counter ++;
-//      }
-//      if (counter == p)
-//      {
-//        counter = 0;
-//        printf("%d ", i);
-//        num[i] = 1;
-//        k ++;
-//      }
-//    } while (k != n);
-//  }
-//}
+      if (!num[i])
+      {
+        counter ++;
+      }
+      if (counter == p)
+      {
+        counter = 0;
+        if (k == n - 1)
+        {
+          printf("%d\n", i);
+        } else
+        {
+          printf("%d ", i);
+        }
+        num[i] = 1;
+        k ++;
+      }
+    } while (k != n);
+  }
+}
