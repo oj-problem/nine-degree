8c87d09d794ba516ed4a4cd057fb1e1c73b710fd
hellfiresong <hellfiresong.me@gmail.com>
2016-02-14 09:21:19 +0800

    Find student message

--- solution1069.c
+++ solution1069.c
@@ -3,67 +3,50 @@
  * Use Linked list data structure
  *
  * hellfire (asyncloading#163.com)
- * Jan 19th, 2016
+ * Feb 14th, 2016
  */
 #include<stdio.h>
-#include<stdlib.h>
 #include<string.h>
+#define MAX_N 1000
 
-
-/* Students structure. */
-typedef struct student {
-  char num[2];         /* Student's number */
-  char name[10];       /* Student's name */
-  char gender[2];      /* Student's gender */
-  int age;             /* Student's age */
-  struct student *next;       /* Pointer field */
-} student;
-
-int main(int argc, char** argv)
+struct student 
 {
+  char num[100];
+  char name[200];
+  char gender[4];
+  int age;
+};
+
+
+int main(int argc, char **argv)
+{
+  struct student stu_arr[MAX_N];
   int n;
-  while(scanf("%d", &n) != EOF)
+  scanf("%d", &n);
+  int i;
+  for (i = 0; i < n; i ++)
   {
-     /* Build linked list */
-     int i;
-     student *head, *p, *q;     
-     head = (student *)malloc(sizeof(student));
-
-     p = head;
-
-     for (i = 0; i < n; i ++)
-     {
-       q = (student *)malloc(sizeof(student));
-       /* Get user input */
-       scanf("%s", q -> num);
-       scanf("%s", q -> name);
-       scanf("%s", q -> gender);
-       scanf("%d", &(q -> age));
-       p -> next = q;
-       q = p;
-     }
-     /* Last Node pointes to NULL */
-     p -> next = NULL;
-
-     /* Linked list search */
-     int m;
-     int j;
-     char str[2];         /* Search number. */
-     scanf("%d", &m);
-     for (j = 0; j < m; j ++)
-     {
-       scanf("%s", str);
-       /* Traverse linked list */
-       while (head -> next != NULL)
-       {
-         printf("%s", head -> next -> num); 
-         if (strcmp(head -> next -> num, str) == 0)
-         {
-           printf("%s %s %s %d\n", head -> next -> num, head -> next -> name, head -> next -> gender, head -> next -> age);
-         }
-         head = head -> next;
-       }
-     } 
+    scanf("%s %s %s %d", stu_arr[i].num, stu_arr[i].name, stu_arr[i].gender, &stu_arr[i].age);
   }
-  return 0;   
+  int m;
+  scanf("%d", &m);
+  for (i = 0; i < m; i ++)
+  {
+    char num[100];
+    scanf("%s", num);
+    int j;
+    int flag = 0;
+    for (j = 0; j < n; j ++)
+    {
+      if (strcmp(num, stu_arr[j].num) == 0)
+      {
+        flag = 1;
+        printf("%s %s %s %d\n", stu_arr[j].num, stu_arr[j].name, stu_arr[j].gender, stu_arr[j].age);
+      }
+    }
+    if (!flag)
+    {
+      printf("No Answer!\n");
+    }
+  }  
 }
